10ebf37949db8fdf011f482cd7802e3bca324d2d
Marco Torres <93018412+MarcoTorresValenzuela@users.noreply.github.com>
2024-07-12 23:08:54 +0000

    Initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+
+$( "#enviarCorreo" ).on( "click", function() {
+  alert( "El correo fue enviado correctamente...." );
+} );
+
+var titulo_1 = $( "#preparacion" ).first();
+titulo_1.on( "dblclick", function() {
+  titulo_1.toggleClass( "subraya_1_color" );
+} );
+
+var titulo_2 = $( "#ingredientes" ).first();
+titulo_2.on( "dblclick", function() {
+  titulo_2.toggleClass( "subraya_2_color" );
+} );
+
+$(".Titulos_cartas").click(function () {
+  $(".contenedor_cartas p").toggle();
+});
+
