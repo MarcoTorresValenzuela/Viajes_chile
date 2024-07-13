4d3cf62eebbd439b7d12d8c9197c0ed56b92bec9
Marco Torres <93018412+MarcoTorresValenzuela@users.noreply.github.com>
2024-07-13 01:55:18 +0000

    Falta arreglar el carrusel y el footer

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
 
-$( "#enviarCorreo" ).on( "click", function() {
+$( "#enviarmensaje" ).on( "click", function() {
   alert( "El correo fue enviado correctamente...." );
 } );
 
